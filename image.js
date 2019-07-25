e0a54e04c079b0f4d64d87e68a488cee87040a94
ajimuktiwijaya <50916244+ajimuktiwijaya@users.noreply.github.com>
2019-07-25 14:48:48 +0700

    Add files via upload

---
+++ image.js
@@ -0,0 +1,6 @@
+<body background="image/malam.jpg"; style="color: white">
+	<center><h1>youtube</h1></center>
+	<li><a target="_blank" href="www.youtube.com">youtube</a></li>
+
+	</table>
+	<center><img src="image/youtube.jpg"width="300px" height="175px">
